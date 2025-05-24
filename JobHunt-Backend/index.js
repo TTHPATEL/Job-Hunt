46a4bc6112120793e6b7df037a4e21541c389e58
Tapasvi Chhabhaiya <117344055+TTHPATEL@users.noreply.github.com>
2025-05-24 11:05:09 +0530

    Add Envirnment Variable

--- JobHunt-Backend/index.js
+++ JobHunt-Backend/index.js
@@ -1,28 +1,25 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from 'cors';
+import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-import "./utils/cloud.js";  
+import "./utils/cloud.js";
 
 dotenv.config({});
 // dotenv.config();
 const app = express();
 
-
-
-
 //Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: [process.env.FRONTEND_URL],
   credentials: true,
 };
 app.use(cors(corsOptions));
@@ -31,12 +28,12 @@ const PORT = process.env.PORT || 5001;
 
 //API's
 
-app.use("/api/user",userRoute);
-app.use("/api/company",companyRoute);
-app.use("/api/job",jobRoute);
-app.use("/api/application",applicationRoute);
+app.use("/api/user", userRoute);
+app.use("/api/company", companyRoute);
+app.use("/api/job", jobRoute);
+app.use("/api/application", applicationRoute);
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   connectDB();
-  console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT}`);
 });
